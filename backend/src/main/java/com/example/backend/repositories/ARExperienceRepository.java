87fb1e31e89781ef54ac934dc818cbb7617e8165
youssefbouhaous <youssefbouhaous01@gmail.com>
2025-03-11 20:57:10 +0000

    deleting vaadin deps and explicitly defining the ar experience path

--- backend/src/main/java/com/example/backend/repositories/ARExperienceRepository.java
+++ backend/src/main/java/com/example/backend/repositories/ARExperienceRepository.java
@@ -1,9 +1,9 @@
 package com.example.backend.repositories;
 
-import com.example.backend.entities.ARExperience;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.backend.entities.ARExperience;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "ar-experiences")
 public interface ARExperienceRepository extends JpaRepository<ARExperience, Long> {
 }
