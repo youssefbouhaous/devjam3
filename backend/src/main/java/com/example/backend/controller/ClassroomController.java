b272e2873f21ff8963358e4638d0abb69072120b
Slimani-amin <amineslimani1610@gmail.com>
2025-03-13 02:01:36 +0000

    add Controller package

---
+++ backend/src/main/java/com/example/backend/controller/ClassroomController.java
@@ -0,0 +1,37 @@
+package com.example.backend.controller;
+
+
+import com.example.backend.DTOS.ARExperienceDTO;
+import com.example.backend.DTOS.ClassroomDTO;
+import com.example.backend.DTOS.TeacherDTO;
+import com.example.backend.entities.Classroom;
+import com.example.backend.service.AppService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/classrooms")
+public class ClassroomController {
+    private AppService appService;
+
+
+
+    @PostMapping("/{classroomId}")
+    public ARExperienceDTO createARExperience(@RequestBody ARExperienceDTO arExperienceDTO, @PathVariable Long classroomId) {
+        return  appService.addARExperience(arExperienceDTO, classroomId);
+    }
+
+    /*@PostMapping("/classrooms")
+    public Classroom addClassroom(@RequestParam String name,
+                                  @RequestParam String description,
+                                  @RequestParam Long teacherId) {
+        Classroom classroom = appService.addClassroom(name, description, teacherId);
+        return  classroom;
+    }*/
+
+}
